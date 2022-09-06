30dbe61bdd2b47720f90e09e15851beadc34a36e
opipi <opipi406@gmail.com>
2022-09-07 01:35:57 +0900

    fix: styleとfooterの修正

--- my-theme-template/footer.php
+++ my-theme-template/footer.php
@@ -1,5 +1,40 @@
 <footer class="customizer__footer_bgcolor">
-  <p id="copyright">&copy; opipi. 2022.</p> 
+  <div class="footer-content">
+    <div class="content">
+      <h2 class="content__title">ABOUT</h2>
+      <ul class="content__list">
+        <li><a href="#">リンク1</a></li>
+        <li><a href="#">リンク2</a></li>
+      </ul>
+    </div>
+    <div class="content">
+      <h2 class="content__title">SUPPORT</h2>
+      <ul class="content__list">
+        <li><a href="#">リンク1</a></li>
+        <li><a href="#">リンク2</a></li>
+        <li><a href="#">FAQ</a></li>
+      </ul>
+    </div>
+    <div class="content">
+      <h2 class="content__title">STORE</h2>
+      <ul class="content__list">
+        <li><a href="#">札幌店</a></li>
+        <li><a href="#">六本木店</a></li>
+        <li><a href="#">渋谷店</a></li>
+        <li><a href="#">横浜店</a></li>
+        <li><a href="#">梅田店</a></li>
+        <li><a href="#">天神店</a></li>
+      </ul>
+    </div>
+    <div class="content">
+      <h2 class="content__title">CONTACT</h2>
+      <p>24時間 年中無休 <br class="sp">フリーダイヤル<br>0120-456-7890</p>
+    </div>
+  </div>
+  <div class="footer-title">
+    <h2>Sample Site</h2>
+  </div>
+  <p id="copyright">&copy; opipi. 2022.</p>
 </footer>
 <?php wp_footer(); ?>
 </body>
--- my-theme-template/template-parts/sample-part.php
+++ my-theme-template/template-parts/sample-part.php
@@ -1,3 +1,3 @@
-<div class="sample-part" style="background: gray; width: 600px; height: 120px; margin: 400px auto; border-radius: 24px;">
+<div class="sample-part" style="background: gray; width: 600px; height: 120px; margin: 300px auto; border-radius: 24px;">
   <p style="text-align: center; line-height: 120px;">This is Sample Template Part.</p>
 </div>
