1d8aa9930b430c9e6a45e3b675d20c2799768033
opipi406 <opipi.work@gmail.com>
2022-11-19 01:26:06 +0900

    fix

---
+++ my-theme-template/template-parts/sample-card.php
@@ -0,0 +1,9 @@
+<div class="card" style="min-height: 300px;">
+  <div class="card__thumb-box">
+    <img class="card__thumb" src="<?php echo img_dir() . '/noimage.jpg' ?>">
+  </div>
+  <div class="card__body">
+    <h2 class="card__title">タイトルです</h2>
+    <p class="card__text">カードコンポーネントです。カードコンポーネントです。カードコンポーネントです。</p>
+  </div>
+</div>
--- my-theme-template/template-parts/sample-part.php
+++
@@ -1,3 +0,0 @@
-<div class="sample-part" style="background: gray; width: 600px; height: 120px; margin: 300px auto; border-radius: 24px;">
-  <p style="text-align: center; line-height: 120px;">This is Sample Template Part.</p>
-</div>
