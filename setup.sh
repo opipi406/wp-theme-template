8844f4805005608cc33cff164b7d111596e6ea25
opipi406 <opipi.work@gmail.com>
2023-02-07 01:03:09 +0900

    add: setup shell script

---
+++ setup.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+docker-compose build
+docker-compose up -d
+
+clear
+echo -n "テーマディレクトリ名を入力: "
+read DIR
+if [ -z $DIR ]; then
+  exit 0
+fi
+
+mv ./my-theme-template ./html/wp-content/themes/$DIR
+echo
+echo "Make theme file."
+echo "-> html/wp-content/themes/$DIR"
+
+rm -rf .git
+rm .gitignore
+echo
+echo "Deleted current git files."
+echo "-> .git/ .gitignore"
+
+cd ./html/wp-content/themes/$DIR
+git init
+
+echo "php_value upload_max_filesize 1024M" >> ./html/.htaccess
+echo "php_value post_max_size 1024M" >> ./html/.htaccess
+echo "php_value memory_limit 256M" >> ./html/.htaccess
+echo "php_value max_execution_time 300" >> ./html/.htaccess
+echo "php_value max_input_time 300" >> ./html/.htaccess
+
+echo
+echo "===================================="
+echo "setup completed."
+echo
+echo "next step"
+echo "$ cd ./html/wp-content/themes/$DIR"
+echo '$ git add .'
+echo '$ git commit -m "first commit"'
+echo '$ git remote add origin <URL>'
+echo '$ git push -uf origin main'
