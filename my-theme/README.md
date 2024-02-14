7c11052cb08f4a327f988a3a13a8ebee4b79d05f
opipi406 <opipi.work@gmail.com>
2024-02-14 23:43:47 +0900

    Update: sass

---
+++ my-theme/README.md
@@ -0,0 +1,51 @@
+# [Project Name]
+
+# Installation
+
+```bash
+git clone https://github.com/opipi406/~
+```
+
+```bash
+npm ci
+```
+
+# Usage
+### Sassのwatcher起動 (sass, autoprefixer)
+```bash
+npm run dev
+```
+### browser-syncを使用してwatcher起動
+```bash
+npm run dev:sync
+```
+### Utilityクラス定義CSSファイルの生成
+```bash
+npm run build:utils
+```
+### Utilityクラス定義CSSファイルの最適化
+```bash
+npm run purge:utils
+```
+### webpackのwatcher起動
+```bash
+npm run dev:js
+```
+### JavaScriptファイルをscript.jsにバンドル
+```bash
+npm run build:js
+```
+### 開発環境にテーマファイルを反映（デプロイ）
+```bash
+npm run deploy:dev
+```
+### 本番環境にテーマファイルを反映（デプロイ）
+```bash
+npm run deploy
+```
+
+# Requirement
+|||
+|-|-|
+|OS|Mac OS|
+|node|v18.17.1|
--- my-theme/footer.php
+++ my-theme/footer.php
@@ -1,40 +1,12 @@
 <footer>
-  <div class="footer-content">
-    <div class="content">
-      <h2 class="content__title">ABOUT</h2>
-      <ul class="content__list">
-        <li><a href="#">リンク1</a></li>
-        <li><a href="#">リンク2</a></li>
-      </ul>
-    </div>
-    <div class="content">
-      <h2 class="content__title">SUPPORT</h2>
-      <ul class="content__list">
-        <li><a href="#">リンク1</a></li>
-        <li><a href="#">リンク2</a></li>
-        <li><a href="#">FAQ</a></li>
-      </ul>
-    </div>
-    <div class="content">
-      <h2 class="content__title">STORE</h2>
-      <ul class="content__list">
-        <li><a href="#">札幌店</a></li>
-        <li><a href="#">六本木店</a></li>
-        <li><a href="#">渋谷店</a></li>
-        <li><a href="#">横浜店</a></li>
-        <li><a href="#">梅田店</a></li>
-        <li><a href="#">天神店</a></li>
-      </ul>
-    </div>
-    <div class="content">
-      <h2 class="content__title">CONTACT</h2>
-      <p>24時間 年中無休 <br class="sp">フリーダイヤル<br>0120-456-7890</p>
+  <div class="footer-wrapper">
+    <a href="#">
+      <img class="footer-logo" src="<?php img_dir("footer-logo.svg"); ?>" class="野村">
+    </a>
+    <div class="footer-menu-wrapper">
+      <!-- <?php wp_nav_menu(['theme_location' => 'footer-menu']); ?> -->
     </div>
   </div>
-  <div class="footer-title">
-    <h2>Sample Site</h2>
-  </div>
-  <p id="copyright">&copy; opipi. 2022.</p>
 </footer>
 <?php wp_footer(); ?>
 </body>
